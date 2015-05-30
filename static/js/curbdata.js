8a5795b07496802f2c32235a085cfb7aaa60a0f3
Nick Bolten <nbolten@gmail.com>
2015-05-29 20:23:52 -0700

    Slight code cleanup, added first attempt at Azure compatibility

--- static/js/curbdata.js
+++ static/js/curbdata.js
@@ -4,19 +4,20 @@ function requestCurbsUpdate(layerGroup, map, api_url) {
     // things up (slowness is due to drawing so many lines)
     layerGroup.clearLayers();
     var bounds = map.getBounds();
+
+    make_circle = function(feature, latlng) {
+      return L.circleMarker(latlng, {
+        'radius': 3,
+        'color': '#0000FF'
+      });
+    };
+
     for (i = 0; i < data.length; i++) {
       var geoJSON = data[i];
-      var coord = geoJSON['coordinates'];
+      var coord = geoJSON.coordinates;
       var latlng = [coord[1], coord[0]];
       if (bounds.contains(latlng)) {
-        point = L.geoJson(geoJSON, {
-          pointToLayer: function(feature, latlng) {
-            return L.circleMarker(latlng, {
-              'radius': 3,
-              'color': '#0000FF'
-            })
-          }
-        });
+        point = L.geoJson(geoJSON, {pointToLayer: make_circle});
 
         //Display info when user clicks on the curb marker
         var popup = L.popup().setContent("<b>Curb Ramp</b>");
