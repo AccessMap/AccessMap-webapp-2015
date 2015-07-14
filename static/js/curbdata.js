b1cd743209c528e1c8f68082d0a2d2093fefcc7e
Nick Bolten <nbolten@gmail.com>
2015-07-14 01:01:52 -0700

    Revert "Revert "Made compatible with new API server (uses GeoJSON)""
    This reverts commit 96681fb8f8572d590d881039a10b2a31974a940c.
    
    Reverting to original geoJSON-compatible commit

--- static/js/curbdata.js
+++ static/js/curbdata.js
@@ -1,23 +1,22 @@
 function requestCurbsUpdate(layerGroup, map, api_url) {
   function drawCurbs(data) {
-    // TODO: turn this into map tiles for several zoom levels to speed
-    // things up (slowness is due to drawing so many lines)
     layerGroup.clearLayers();
     var bounds = map.getBounds();
 
-    make_circle = function(feature, latlng) {
-      return L.circleMarker(latlng, {
+    make_circle = function(feature, latlon) {
+      coords = feature.geometry.coordinates;
+      return L.circleMarker(latlon, {
         'radius': 3,
         'color': '#0000FF'
       });
     };
 
-    for (i = 0; i < data.length; i++) {
-      var geoJSON = data[i];
-      var coord = geoJSON.coordinates;
+    for (i = 0; i < data.features.length; i++) {
+      var feature = data.features[i];
+      var coord = feature.geometry.coordinates;
       var latlng = [coord[1], coord[0]];
       if (bounds.contains(latlng)) {
-        point = L.geoJson(geoJSON, {pointToLayer: make_circle});
+        point = L.geoJson(feature, {pointToLayer: make_circle});
 
         //Display info when user clicks on the curb marker
         var popup = L.popup().setContent("<b>Curb Ramp</b>");
@@ -32,7 +31,7 @@ bounds = map.getBounds().toBBoxString();
 // Request data
 $.ajax({
   type: 'GET',
-  url: api_url + '/curbs.json',
+  url: api_url + '/curbs.geojson',
   data: {
     bbox: bounds
   },
